3c86fc377a72c08123e5c15012b786b931630707
thecitrusdalv <101547139+thecitrusdalv@users.noreply.github.com>
2022-11-04 13:44:01 +0500

    Add files via upload

---
+++ IsKPeriodic.cpp
@@ -0,0 +1,16 @@
+#include "IsKPeriodic.h"
+
+bool IsKPeriodic (const std::string &str, size_t multiple)
+{
+	if (!multiple || str.empty() || str.size() % multiple)
+		return false;
+
+	size_t i = multiple, j = 0;
+
+	while (i < str.size()) {
+		if (str[j] != str[i])
+			return false;
+		j++; i++;
+	}
+	return true;
+}
---
+++ IsKPeriodic.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <string>
+#include <cstddef>
+
+bool IsKPeriodic (const std::string&, size_t);
