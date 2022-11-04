e98867e0eaed5e1bbc4248956a7b5b8dd09a4e22
thecitrusdalv <101547139+thecitrusdalv@users.noreply.github.com>
2022-11-04 14:01:24 +0500

    Update IsKPeriodic.cpp

--- IsKPeriodic.cpp
+++ IsKPeriodic.cpp
@@ -5,13 +5,10 @@ bool IsKPeriodic (const std::string &str, size_t multiple)
 	if (!multiple || str.empty() || str.size() % multiple)
 		return false;
 
-	size_t i = multiple, j = 0;
-
-	while (i < str.size()) {
-		if (str[j] != str[i])
+	for (size_t i = 0; multiple < str.size(); i++, multiple++) {
+		if (str[i] != str[multiple])
 			return false;
-		j++; i++;
 	}
-	
+		
 	return true;
 }
